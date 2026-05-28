42db8cf6f5ced602ee90ee41848ad9b3e2c3c908
Surya Chakraborty <suryachakraborty.dev@gmail.com>
2026-05-28 13:00:42 +0530

    db: resolve mongo dns issue

--- server/src/db/connection.ts
+++ server/src/db/connection.ts
@@ -1,6 +1,11 @@
 import mongoose from 'mongoose'
 import { MONGO_URL } from '../config/env'
 
+// Mongoose dns connection problem fix
+const dns = require('dns')
+
+dns.setServers(['8.8.8.8', '1.1.1.1'])
+
 const DBConnect = async () => {
     try{
         await mongoose.connect(MONGO_URL as string)
